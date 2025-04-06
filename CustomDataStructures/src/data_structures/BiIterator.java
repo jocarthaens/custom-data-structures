c0b6ced5a3c1a93199dc4f896ffe45b098682755
jocarthaens <jocarthaens50@gmail.com>
2025-04-06 17:34:14 +0800

    Update BiIterator.java

--- CustomDataStructures/src/data_structures/BiIterator.java
+++ CustomDataStructures/src/data_structures/BiIterator.java
@@ -2,6 +2,9 @@ package data_structures;
 
 import java.util.Iterator;
 
+// A BiIterator can iterate elements either forward or backward.
+// This iterator is used in FlatMap and DynamicArray data structures.
+
 public interface BiIterator<T> extends Iterator<T> {
 	
     boolean hasNext();
